b7bbb785ceefa9c597a7743c2d4c1013208daafd
SGyldenkvist <silke@gyldenkvist.dk>
2026-09-10 13:20:14 +0200

    Feat: Add login and search endpoints

--- go/cmd/maxi-knows/main.go
+++ go/cmd/maxi-knows/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -17,6 +18,43 @@ func main(){
 		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "Register")
+	})
+
+	http.HandleFunc("GET /login", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Login")
+
+	})
+
+
+	http.HandleFunc("GET /api/search", func(w http.ResponseWriter, r *http.Request){
+		q := r.URL.Query().Get("q")
+
+		language := r.URL.Query().Get("language")
+
+		//Replace with actual result from db later:
+		_= q
+		_= language
+
+		if !r.URL.Query().Has("q"){
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnprocessableEntity)
+
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"statusCode": 422,
+				"message": "q is required",
+			})
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			//replace with actual matches later:
+			"data": []interface{}{},
+		})
 	})
 
 	fmt.Println("Server running on http://localhost:8080")
