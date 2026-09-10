52921630825311159aa8e096d6c53d6da808dfcd
SGyldenkvist <silke@gyldenkvist.dk>
2026-09-10 11:22:15 +0200

    Feat: Set up basic Go http server

--- go/cmd/maxi-knows/main.go
+++ go/cmd/maxi-knows/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintln(w, "WhoKnows")
+	})
+
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
