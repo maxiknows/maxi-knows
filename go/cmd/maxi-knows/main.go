eac64819c4cf63f90001d5cf765d73145791b821
SGyldenkvist <silke@gyldenkvist.dk>
2026-09-10 12:18:20 +0200

    Feat: Add register get endpoint

--- go/cmd/maxi-knows/main.go
+++ go/cmd/maxi-knows/main.go
@@ -12,6 +12,13 @@ func main(){
 		fmt.Fprintln(w, "WhoKnows")
 	})
 
+
+	http.HandleFunc("GET /register", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Register")
+	})
+
 	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
