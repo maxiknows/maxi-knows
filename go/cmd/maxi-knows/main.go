5eff33567a5d16405234a693229d367387547297
SGyldenkvist <silke@gyldenkvist.dk>
2026-09-10 20:33:46 +0200

    feat: Add login and logout endpoints

--- go/cmd/maxi-knows/main.go
+++ go/cmd/maxi-knows/main.go
@@ -6,44 +6,42 @@ import (
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request){
+func main() {
+	http.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "WhoKnows")
 	})
 
-
-	http.HandleFunc("GET /register", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("GET /register", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "Register")
 	})
 
-	http.HandleFunc("GET /login", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("GET /login", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "Login")
 
 	})
 
-
-	http.HandleFunc("GET /api/search", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("GET /api/search", func(w http.ResponseWriter, r *http.Request) {
 		q := r.URL.Query().Get("q")
 
 		language := r.URL.Query().Get("language")
 
 		//Replace with actual result from db later:
-		_= q
-		_= language
+		_ = q
+		_ = language
 
-		if !r.URL.Query().Has("q"){
+		if !r.URL.Query().Has("q") {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusUnprocessableEntity)
 
 			json.NewEncoder(w).Encode(map[string]interface{}{
 				"statusCode": 422,
-				"message": "q is required",
+				"message":    "q is required",
 			})
 			return
 		}
@@ -57,12 +55,10 @@ func main(){
 		})
 	})
 
-
-	http.HandleFunc("POST /api/register", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("POST /api/register", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 
-		
-		if !r.Form.Has("username") || !r.Form.Has("email") || !r.Form.Has("password"){
+		if !r.Form.Has("username") || !r.Form.Has("email") || !r.Form.Has("password") {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusUnprocessableEntity)
 
@@ -75,7 +71,40 @@ func main(){
 
 		json.NewEncoder(w).Encode(map[string]interface{}{
 			"statusCode": 200,
-			"message": "Registered",
+			"message":    "Registered",
+		})
+
+	})
+
+	http.HandleFunc("POST /api/login", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+
+		if !r.Form.Has("username") || !r.Form.Has("password") {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnprocessableEntity)
+
+			json.NewEncoder(w).Encode(map[string]interface{}{})
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"statusCode": 200,
+			"message":    "Logged in",
+		})
+
+	})
+
+	http.HandleFunc("GET /api/logout", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"statusCode": 200,
+			"message":    "Logged out",
 		})
 
 	})
